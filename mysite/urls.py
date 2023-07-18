c1538c890fd1e6c98dd2fb5a33ca135e9be36249
Vincenzo Fehring <vincenzo.fehring@me.com>
2023-07-18 02:33:41 -0500

    Added URL path for media files, now serving from Railway volumes

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf import settings
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.static import serve
 from django.conf.urls.static import static
 
 urlpatterns = [
@@ -23,5 +24,4 @@ urlpatterns = [
     path("", include("posts.urls")),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [ re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT})]
